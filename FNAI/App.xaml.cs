c11aba21a958f84e5565d8871e6675941626c873
Tom WAGNER <tom.wagner@iut-dijon.u-bourgogne.fr>
2026-06-05 20:31:52 +0200

    Add files via upload

---
+++ FNAI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FNAI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ FNAI/Option.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace FNAI
+{
+    public partial class Option : Window
+    {
+        public Option()
+        {
+            InitializeComponent();
+        }
+        private void Block1Left_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Block1Right_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Block2Left_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Block2Right_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Block3Left_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Block3Right_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
