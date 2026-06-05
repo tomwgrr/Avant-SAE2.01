567eccd77b271367dda510ae00ea08abadf349c3
Tom WAGNER <tom.wagner@iut-dijon.u-bourgogne.fr>
2026-06-05 20:31:52 +0200

    Add files via upload

---
+++ FNAI/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FNAI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void End(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OpenSettings(object sender, RoutedEventArgs e)
+        {
+            Option options = new Option();
+            options.Show();
+        }
+
+        private void Selected(object sender, MouseEventArgs e)
+        {
+            switch(sender)
+            {
+                case Button button when button == startButton : startButton.Content = ">Play"; break;
+                case Button button when button == loadButton : loadButton.Content = ">Load"; break;
+                case Button button when button == optionsButton : optionsButton.Content = ">Options"; break;
+                case Button button when button == exitButton : exitButton.Content = ">Exit"; break;
+            }
+        }
+
+        private void Unselect(object sender, MouseEventArgs e)
+        {
+            switch(sender)
+            {
+                case Button button when button == startButton : startButton.Content = "Play"; break;
+                case Button button when button == loadButton : loadButton.Content = "Load"; break;
+                case Button button when button == optionsButton : optionsButton.Content = "Options"; break;
+                case Button button when button == exitButton : exitButton.Content = "Exit"; break;
+            }
+        }
+    }
+}
