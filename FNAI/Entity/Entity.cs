3d6b0fdc27e3b3080a16183563cdbf0fee9951e1
Tom Wagner <tom.wagner@iut-dijon.u-bourgogne.fr>
2026-06-06 14:54:19 +0200

    adding Entity class

---
+++ FNAI/Entity/Entity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNAI.Entity
+{
+    public abstract class Entity
+    {
+        #region --- Attributs ---
+        private int _AISCore;
+        private bool isAwake;
+        #endregion
+
+        public Entity(int AIscore)
+        {
+           this._AISCore = AIscore;
+        }
+        public bool IsAwake { get; set; }
+        public abstract  void DoAnAction();
+        public abstract void Move();
+        public abstract void Attack();
+        public abstract void Noise();
+        public abstract void jumpscare();
+        public void Kill()
+        { 
+            jumpscare();
+        }
+
+
+    }
+}
